46b1e52e5cc04a3632d6a47b80b127a5c3f19e4e
Aidan King <aidan.king151@gmail.com>
2020-06-25 15:40:54 +0100

    add auth service

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,17 +1,16 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class LoginFormComponent extends Component {
   @tracked
   userId = null;
 
+  @service auth;
+
   get isDisabled() {
     return !this.userId;
-  }
-
-  loginAsUserWithId(val) {
-    console.log('UserId: ', val);
   }
 
   @action
@@ -24,6 +23,7 @@ export default class LoginFormComponent extends Component {
     evt.preventDefault();
     const { target } = evt;
     const val = target.querySelector('select').value;
-    this.loginAsUserWithId(val);
+
+    this.auth.loginWithUserId(val);
   }
 }
