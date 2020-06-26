2b437fb5eb5f399f1b4c80f1389e30f89e1d5082
Aidan King <aidan.king151@gmail.com>
2020-06-26 13:29:51 +0100

    addMessage

---
+++ app/components/channel-footer.js
@@ -0,0 +1,24 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class ChannelFooterComponent extends Component {
+  @tracked
+  body = '';
+
+  get isDisabled() {
+    return !this.body;
+  }
+
+  @action
+  updateMessageBody(evt) {
+    this.body = evt.target.value;
+  }
+
+  @action
+  async handleSubmit(evt) {
+    evt.preventDefault();
+    await this.args.sendMessage(this.body);
+    this.body = '';
+  }
+}
--- app/components/chat-container.js
+++ app/components/chat-container.js
@@ -1,11 +1,14 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import fetch from 'fetch';
 
 export default class ChatContainerComponent extends Component {
   @tracked
   messages = [];
+
+  @service auth;
 
   @action
   async loadMessages() {
@@ -16,5 +19,32 @@ export default class ChatContainerComponent extends Component {
     const resp = await fetch(`/api/teams/${teamId}/channels/${id}/messages`);
 
     this.messages = await resp.json();
+  }
+
+  @action
+  async createMessage(body) {
+    const {
+      channel: { id: channelId, teamId },
+    } = this.args;
+    const userId = this.auth.currentUserId;
+    const resp = await fetch('/api/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        teamId,
+        channelId,
+        userId,
+        body,
+      }),
+    });
+
+    if (!resp.ok) {
+      throw Error('Could not save chat message');
+    }
+    const messageData = await resp.json();
+    const user = await (await fetch(`/api/users/${userId}`)).json();
+    this.messages = [...this.messages, { ...messageData, user }];
   }
 }
