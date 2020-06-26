41a7e5e219c748d91ffd810827c3b251a966f039
Aidan King <aidan.king151@gmail.com>
2020-06-26 12:00:38 +0100

    load message data

---
+++ tests/integration/components/chat-container-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | chat-container', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    assert.expect(0);
+  });
+});
